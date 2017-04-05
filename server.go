2a11b42f176ed5ddd09f8b98852ac64cb91509ba
BowenChen <bowenchen886@gmail.com>
2017-04-05 15:42:22 +0800

    add public resources

--- server.go
+++ server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -11,17 +10,15 @@ import (
 
 func main() {
 	port := os.Getenv("PORT")
-
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
-	})
+	mux.Handle("/", http.FileServer(http.Dir("public")))
 
-	n := negroni.Classic() // Includes some default middlewares
+	// Includes some default middlewares
+	n := negroni.Classic()
 	n.UseHandler(mux)
 	n.Run(":" + port)
 }
